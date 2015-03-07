e3ef84b00dd45f05c9bce17db971c3bbc0adfc18
Ekvi <dima_sh84@mail.ru>
2015-03-07 13:37:49 +0200

    update ScreenSizeChecker

--- src/main/java/com/exchangerates/client/views/ScreenSizeChecker.java
+++ src/main/java/com/exchangerates/client/views/ScreenSizeChecker.java
@@ -19,7 +19,7 @@ public class ScreenSizeChecker extends Widget {
     public int getChartHeight() {
         int height;
 
-        if(Window.getClientHeight() <= 960 && Window.getClientHeight() >= 890) {
+        if(Window.getClientHeight() <= 980 && Window.getClientHeight() >= 890) {
             height = 180;
         } else if(Window.getClientHeight() <= 870 && Window.getClientHeight() >= 795) {
             height = 170;
@@ -54,32 +54,28 @@ public class ScreenSizeChecker extends Widget {
 
     public String getScrollPanelHeight() {
         String height = "";
-        if(Window.getClientHeight() > 940 && Window.getClientHeight() < 960 ) {
-            height = "503px";
-        } else if(Window.getClientHeight() > 920 && Window.getClientHeight() < 940 ) {
+        if(Window.getClientHeight() > 910 && Window.getClientHeight() < 960 ) {
             height = "500px";
-        } else if(Window.getClientHeight() > 890 && Window.getClientHeight() < 910 ) {
+        } else if(Window.getClientHeight() > 880 && Window.getClientHeight() < 910 ) {
             height = "473px";
         } else if(Window.getClientHeight() > 855 && Window.getClientHeight() < 870 ) {
             height = "440px";
-        } else if(Window.getClientHeight() > 830 && Window.getClientHeight() < 855 ) {
+        } else if(Window.getClientHeight() > 820 && Window.getClientHeight() < 855 ) {
             height = "420px";
         } else if(Window.getClientHeight() > 795 && Window.getClientHeight() < 810 ) {
             height = "380px";
         } else if(Window.getClientHeight() > 760 && Window.getClientHeight() < 790 ) {
             height = "350px";
-        } else if(Window.getClientHeight() > 740 && Window.getClientHeight() < 750 ) {
-            height = "318px";
-        } else if(Window.getClientHeight() > 720 && Window.getClientHeight() < 730 ) {
-            height = "315px";
-        } else if(Window.getClientHeight() > 695 && Window.getClientHeight() < 710 ) {
+        } else if(Window.getClientHeight() > 695 && Window.getClientHeight() < 750 ) {
             height = "320px";
-        } else if(Window.getClientHeight() > 660 && Window.getClientHeight() < 690) {
+        } else if(Window.getClientHeight() > 660 && Window.getClientHeight() < 695) {
             height = "290px";
-        } else if(Window.getClientHeight() > 635 && Window.getClientHeight() < 660) {
+        } else if(Window.getClientHeight() > 630 && Window.getClientHeight() < 660) {
             height = "265px";
         } else if(Window.getClientHeight() > 615 && Window.getClientHeight() < 630) {
             height = "230px";
+        } else if(Window.getClientHeight() > 580 && Window.getClientHeight() < 615) {
+            height = "225px";
         }
 
         return height;
@@ -91,30 +87,42 @@ public class ScreenSizeChecker extends Widget {
             height = Window.getClientHeight()/1.61 + "px";
         } else if(Window.getClientHeight() > 920 && Window.getClientHeight() < 940 ) {  //1280x1024 1600x1024
             height = Window.getClientHeight()/1.57 + "px";
+        } else if(Window.getClientHeight() > 910 && Window.getClientHeight() < 920 ) {
+            height = Window.getClientHeight()/1.55 + "px";
         } else if(Window.getClientHeight() > 890 && Window.getClientHeight() < 910 ) {
             height = Window.getClientHeight()/1.52 + "px";
+        } else if(Window.getClientHeight() > 880 && Window.getClientHeight() < 890 ) {
+            height = Window.getClientHeight()/1.50 + "px";
         } else if(Window.getClientHeight() > 855 && Window.getClientHeight() < 870 ) {  //1280x960
             height = Window.getClientHeight()/1.46 + "px";
         } else if(Window.getClientHeight() > 830 && Window.getClientHeight() < 855 ) {  //1280x960
             height = Window.getClientHeight()/1.40 + "px";
+        } else if(Window.getClientHeight() > 820 && Window.getClientHeight() < 830 ) {  //1280x960  big icons
+            height = Window.getClientHeight()/1.38 + "px";
         } else if(Window.getClientHeight() > 795 && Window.getClientHeight() < 810 ) {  //1600x900
             height = Window.getClientHeight()/1.36 + "px";
         } else if(Window.getClientHeight() > 760 && Window.getClientHeight() < 790 ) {  //1152x864
             height = Window.getClientHeight()/1.3 + "px";
         } else if(Window.getClientHeight() > 740 && Window.getClientHeight() < 750 ) {  //1680x1050 125%
             height = Window.getClientHeight()/1.27 + "px";
+        } else if(Window.getClientHeight() > 730 && Window.getClientHeight() < 740 ) {  //1152x864 big icons
+            height = Window.getClientHeight()/1.25 + "px";
         } else if(Window.getClientHeight() > 720 && Window.getClientHeight() < 730 ) {  //1600x1024 125%
             height = Window.getClientHeight()/1.24 + "px";
         } else if(Window.getClientHeight() > 695 && Window.getClientHeight() < 710 ) {   //1280x800
             height = Window.getClientHeight()/1.19 + "px";
-        } else if(Window.getClientHeight() > 660 && Window.getClientHeight() < 690 ) {  //1366x768  1360x768  1280x768
+        } else if(Window.getClientHeight() > 660 && Window.getClientHeight() < 695 ) {  //1366x768  1360x768  1280x768
             height = Window.getClientHeight()/1.135 + "px";
         } else if(Window.getClientHeight() > 650 && Window.getClientHeight() < 660 ) {
             height = Window.getClientHeight()/1.1 + "px";
         } else if(Window.getClientHeight() > 635 && Window.getClientHeight() < 650 ) {
             height = Window.getClientHeight()/1.08 + "px";
+        } else if(Window.getClientHeight() > 630 && Window.getClientHeight() < 635 ) {
+            height = Window.getClientHeight()/1.07 + "px";
         } else if(Window.getClientHeight() > 615 && Window.getClientHeight() < 630 ) {
             height = Window.getClientHeight()/1.05 + "px";
+        } else if(Window.getClientHeight() > 580 && Window.getClientHeight() < 615 ) {
+            height = Window.getClientHeight() + "px";
         }
         return height;
     }
